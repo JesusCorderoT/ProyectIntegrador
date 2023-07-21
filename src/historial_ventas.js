d88930e7103f07114a61a04806f749cbb80a211f
anudsa <anwardelgadosilva@gmail.com>
2023-07-21 14:26:56 -0600

    Se crean entidades de tarjeta y marca

--- src/historial_ventas.js
+++ src/historial_ventas.js
@@ -1,6 +1,5 @@
-
 //Arreglo que contiene datos muestra.
-arregloDatos = [
+let arregloDatos = [
     {'fecha': '06/28/2023', 'cliente': 'Walter', 'productos': 'Técnica Clásica', 'total': '$50'},
     {'fecha': '06/30/2023', 'cliente': 'Jesse', 'productos': 'Lash Shampoo', 'total': '$80'},
     {'fecha': '06/29/2023', 'cliente': 'Saul', 'productos': 'Lash Brush Desechables', 'total': '$65'},
@@ -13,21 +12,50 @@ arregloDatos = [
     {'fecha': '06/20/2023', 'cliente': 'Tuco', 'productos': 'Cepillo de Limpieza', 'total': '$45'},
     {'fecha': '06/17/2023', 'cliente': 'Walt Jr.', 'productos': 'Retiro de pestañas', 'total': '$60'},
 ]
-//Función que llena dinámicamente la tabla
-function insertarDatosTabla(datos){
-    let tabla = document.getElementById("celdasTabla");
-    for(let i=0; i<datos.length; i++){
-        let fila =  `<tr>
-                        <td scope="row">${datos[i].fecha}</th>
-                        <td scope="row">${datos[i].cliente}</td>
-                        <td scope="row">${datos[i].productos}</td>
-                        <td scope="row">${datos[i].total}</td>
-                    </tr>`
-        tabla.innerHTML+=fila;
-    }
 
+// Función que llena dinámicamente la tabla
+function insertarDatosTabla(datos) {
+    let tabla = document.getElementById("celdasTabla");
+    tabla.innerHTML = " ";
+    for (let i = 0; i < datos.length; i++) {
+        let fila = `<tr>
+                    <td scope="row">${datos[i].fecha}</th>
+                    <td scope="row">${datos[i].cliente}</td>
+                    <td scope="row">${datos[i].productos}</td>
+                    <td scope="row">${datos[i].total}</td>
+                </tr>`;
+        tabla.innerHTML += fila;
+    }
+}
+let datos = arregloDatos; // se inicializa con los datos originales
+//Función que lee lo que se introduce en la búsqueda
+function readSearch(){
+    const input = document.querySelector('input[type="text"]');
+    
+    input.addEventListener("keyup", () => {
+        let value=input.value.toLowerCase();
+        console.log(`The term searched for was ${value}`);
+        datos = filtrarTabla(value,arregloDatos); //se guardan los datos filtrados en datos
+        //Se llama la función de insertar tablar para añadir los datos filtrados
+        console.log(`The filter was ${JSON.stringify(datos)}`);
+        insertarDatosTabla(datos);
+    });   
+}
+function filtrarTabla(value,datos){
+    let datosFiltrados=[];
+
+    for(let i=0; i<datos.length;i++){
+        valor=value.toLowerCase();
+        let cliente=datos[i].cliente.toLowerCase();
+        if(cliente.includes(valor)){
+            datosFiltrados.push(datos[i]);
+        }
+    }
+    
+    return datosFiltrados;
 }
 
-insertarDatosTabla(arregloDatos);
-
-
+readSearch();
+insertarDatosTabla(datos);
+// insertarDatosTabla(dummyData);
+// console.log(dummyData);
